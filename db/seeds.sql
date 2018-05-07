cb6b7202cae9ecbcc732256626d477d28bdfb901
unknown <webd3vdave@gmail.com>
2018-05-06 17:59:55 -0700

    code mostly complete, ready for testing.

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO `hamburgertown`.`burgers` (`burger_name`, `devoured`) VALUES ('Double Cheeseburger', '0');
+INSERT INTO `hamburgertown`.`burgers` (`burger_name`, `devoured`) VALUES ('Bacon Jalapeno Burger', '0');
+INSERT INTO `hamburgertown`.`burgers` (`burger_name`, `devoured`) VALUES ('Avocado Ranch Burger', '0');
